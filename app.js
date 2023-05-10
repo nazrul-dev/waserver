ceed50a398fa1a3cf57a23343ad88abcf3c7b3e1
Nazrul <nazrul.dev@gmail.com>
2023-05-10 20:36:52 +0800

    update

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const port = parseInt(process.env.WA_SERVER_PORT ?? 8000)
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use('/', routes)
-
+// app.use('/', routes)
+app.use('/', (req, res) => {
+    res.json({messegae:"running"})
+})
 const listenerCallback = () => {
     init()
     console.log(`Server is listening ${port}`)
